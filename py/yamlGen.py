4b3383f3f9b19fe456a0fcf63f41b8492eb769f7
Shizheng <housz@nus.edu.sg>
2023-07-27 14:09:04 +0000

    update: total 10000 accounts and docker swarm mode

--- py/yamlGen.py
+++ py/yamlGen.py
@@ -14,6 +14,12 @@ for i in range(1,number_node+1):
     ws_port = str(33444+i)
     node['hostname']='node_'+str(i)
     node['image']='ethereum/client-go:release-1.10'
+    worker_node_id = ''
+    if i < 9:
+        worker_node_id = 'worker-00'+str(i+1)
+    elif i < 99:
+        worker_node_id = 'worker-0'+str(i+1)
+    node['deploy']={'placement':{'constraints':['node.hostname=='+worker_node_id]}}
     node['command']='--datadir  /root/data --nodiscover --syncmode full --nodekey /root/files/priv.key --port '+str(http_port)+' --http --http.addr "0.0.0.0" --http.vhosts="*" --http.corsdomain="*" --http.port '+str(port)+' --http.api db,eth,net,web3,admin,personal,miner,signer:insecure_unlock_protect  --networkid '+str(chain_id)+' --unlock 0 --password /root/files/password --mine --allow-insecure-unlock  --ws --ws.port '+str(ws_port)+' --ws.addr "0.0.0.0" --ws.origins="*" --ws.api eth,net,web3'
     volumes=[]
     volumes.append('./nodes/node_'+str(i)+'/keys/password:/root/files/password:ro')
